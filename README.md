954ce3952a89cfd433aa05a8efb3c13343e50ff6
Isaiah Kang <i.say.yaa@gmail.com>
2022-04-22 11:36:53 -0400

    final almost done

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ json-server --watch db.json --port 3004
 to start the server that connects our json file.
 
 
-MAKE SURE TO ZOOM IN 75%...when it does full screen mode.
+MAKE SURE TO ZOOM IN 50 to 75%...when it does full screen mode.
 
 This project is done by Emmanuel and Isaiah from NYC Flatiron School March 2022 cohorts.
 
--- src/components/About.js
+++ src/components/About.js
@@ -5,10 +5,13 @@ const About = () => {
         <div>
             <h1></h1>
             <img className='snorlex' src="https://i.pinimg.com/originals/4d/88/eb/4d88ebc2b8b3a26b8d698ff189f340b3.png" alt="snorlex" />
-            <p className="aboutstyle">Created by.Emmanuel Boateng and Isaiah Kang</p>
+            <p className="aboutstyle">Created by                           </p>
+            <p id="spacing">and</p>
             <img className="linkedinlogo" src="https://www.freeiconspng.com/thumbs/linkedin-logo-png/linkedin-9.png"/>
             <img className="githublogo" src="https://www.picng.com/upload/github/png_github_64040.png"/>
-            <a ref="" />
+            <a className="emm" href="https://www.linkedin.com/in/emmanuel-boateng-57a64942/">Emmanuel Boateng</a>
+            <a className="isa" href="https://www.linkedin.com/in/isaiah-kang93/">Isaiah Kang</a>
+            
             
             
         </div>
